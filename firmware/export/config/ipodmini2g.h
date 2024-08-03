0ebfb948087b1ec4c6abd7163b2fa3251b277846
Olsro <161404379+Olsro@users.noreply.github.com>
2024-08-03 18:06:46 +0200

    iPodMini1G2G : Enable flash storage

--- firmware/export/config/ipodmini2g.h
+++ firmware/export/config/ipodmini2g.h
@@ -114,7 +114,7 @@
 
 /* define this if you have a disk storage, i.e. something
    that needs spinups and can cause skips when shaked */
-#define HAVE_DISK_STORAGE
+#define HAVE_FLASH_STORAGE
 
 /* Define this for LCD backlight available */
 #define HAVE_BACKLIGHT
